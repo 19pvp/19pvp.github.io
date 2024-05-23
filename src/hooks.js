401c5ecd8ba5f060f41039711f67310d791d3482
Clement Denis <le.mikmac@gmail.com>
2024-05-23 23:19:32 +0100

    Home made tooltips, freshly baked.

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,22 @@
 import { useState, useEffect } from 'preact/hooks'
 
+export const useEvent = (eventName, opts = false) => {
+  const [event, setEvent] = useState({})
+  useEffect(() => {
+    addEventListener(eventName, setEvent, opts)
+    return () => removeEventListener(eventName)
+  }, [eventName, opts])
+  return event
+}
+
+export const useWindowSize = () => {
+  useEvent('resize') // We don't need the event, just to retrigger
+  return {
+    innerWidth: window.innerWidth,
+    innerHeight: window.innerHeight,
+  }
+}
+
 const fetchBody = async (url, fetchOpts) => {
   const state = {}
   try {
@@ -27,15 +44,20 @@ export const useFetchJSON = (...args) => {
   return state
 }
 
+const cache = new Map()
 export const useFetch = (url, fetchOpts, inputs = [url]) => {
-  const [state, setState] = useState({ isLoading: true })
+  const withCache = (!fetchOpts?.method || fetchOpts.method === 'GET')
+  const initialState = (withCache && cache.get(url)) || { isLoading: true }
+  const [state, setState] = useState(initialState)
   // biome-ignore lint/correctness/useExhaustiveDependencies: dependencies defined by the caller
   useEffect(() => {
     if (url == null) return
     const controller = new AbortController()
     const opts = { ...fetchOpts, signal: controller.signal }
     fetchBody(url, opts).then((nextState) => {
-      controller.signal.aborted || setState(nextState)
+      if (controller.signal.aborted) return
+      withCache && !nextState.error && cache.set(url, { ...nextState, fromCache: true })
+      setState(nextState)
     })
     state.isLoading || setState({ ...state, isFetching: true })
     return () => controller.abort()
