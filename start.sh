856f9ed10c50b6b470e7a734ab26502e2fdd6efe
Clement Denis <clement@01talent.com>
2026-06-29 22:47:31 +0200

    add auto-reload

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 export PATH="/root/.deno/bin:$PATH"
 deno task --env-file config:install
-deno serve --env-file --allow-env --allow-net --allow-read --allow-write --allow-run --port=$PORT service/server.ts
+deno serve --watch=service,config --env-file --allow-env --allow-net --allow-read --allow-write --allow-run --port=$PORT service/server.ts
