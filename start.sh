57f841d311865a8921542ecdf60c8f8bd01083cd
Clement Denis <clement@01talent.com>
2026-06-29 08:37:40 +0200

    pass port

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-/root/.deno/bin/deno serve --env-file --allow-env --allow-net --allow-read --allow-write --allow-run service/server.ts
+/root/.deno/bin/deno serve --env-file --allow-env --allow-net --allow-read --allow-write --allow-run --port=$PORT service/server.ts
